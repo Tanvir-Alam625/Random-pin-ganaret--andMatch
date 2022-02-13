4b3fde5d8d888379e87cf4f5b90a2d3d0de9dd3a
tanveer <mdtanviralamht731@gmail.com>
2022-02-13 20:45:37 +0600

    random pin generated

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-// pin generate function 
+//get pin  function 
+
 function generatePin(){
-    
+    const randomPin = getRandomPin() 
+    document.getElementById('display-pin').value = randomPin;
+}
+// random pin generate function
+function getRandomPin(){
+    const pin = Math.round(Math.random() * 100000);
+    const covertPin = pin+ '';
+    if(covertPin.length == 5){
+        return pin;
+    }else{
+        return getRandomPin()
+    }
+
 }
